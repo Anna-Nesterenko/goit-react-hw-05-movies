70a983bb00400095dc213acd57edf6551ed5880a
AnnaNesterenko <nura_arh@ukr.net>
2022-08-24 15:15:21 +0300

    addAllLogic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { SideBar } from 'components';
 import { GlobalStyles } from './GlobalStyles';
 import { ToastContainer } from 'react-toastify';
